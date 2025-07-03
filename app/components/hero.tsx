0977fd2afba265853c8f244bb401734d29edb216
Salman-S7 <sammyzed1505@gmail.com>
2025-07-04 01:45:08 +0530

    updated images

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,8 +1,4 @@
-"use client";
-
 import Image from "next/image";
-import AnimatedAnalyticsCard from "./animated-card";
-import AnimatedAreaChart from "./animated-chart";
 
 const HeroSection = () => {
   return (
